d1cbef31bb349c50965270b84477a34fbc76cf88
Danila Slesarev <blackdeer1524@yandex.ru>
2022-10-21 12:50:03 +0300

    1.9 done

---
+++ 1/9/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+
+void revarray(void *base, size_t nel, size_t width) {
+    if (base == NULL || nel == 1) {
+        return;
+    }
+
+    void *intermediate_buffer = malloc(width);
+    void *right_item = base + (nel - 1) * width;
+    for (size_t i = 0; i < nel / 2; ++i) {
+        memcpy(intermediate_buffer, base, width);
+        memcpy(base, right_item, width);
+        memcpy(right_item, intermediate_buffer, width);
+        base += width;
+        right_item -= width;
+    }
+    free(intermediate_buffer);
+}
+
+int main() {
+    double even_test_array[] = {3.F, 6.F, 10.F, 13.F};
+    int odd_test_array[] = {3, 6, 10, 13, 15};
+
+    size_t even_base_type_width = sizeof (even_test_array[0]);
+    size_t odd_base_type_width = sizeof (odd_test_array[0]);
+
+    size_t even_test_array_length = sizeof (even_test_array) / even_base_type_width;
+    size_t odd_test_array_length = sizeof (odd_test_array) / odd_base_type_width;
+
+    for (size_t i = 0; i < even_test_array_length; ++i) {
+        printf("%lf ", even_test_array[i]);
+    }
+    printf("\n");
+    revarray(even_test_array, even_test_array_length, even_base_type_width);
+    for (size_t i = 0; i < even_test_array_length; ++i) {
+        printf("%lf ", even_test_array[i]);
+    }
+    printf("\n");
+
+    for (size_t i = 0; i < odd_test_array_length; ++i) {
+        printf("%d ", odd_test_array[i]);
+    }
+    revarray(odd_test_array, odd_test_array_length, odd_base_type_width);
+    printf("\n");
+    for (size_t i = 0; i < odd_test_array_length; ++i) {
+        printf("%d ", odd_test_array[i]);
+    }
+    printf("\n");
+}
