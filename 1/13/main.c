cc3a9f677a5558add780afa820236e3e41b4a1c0
Danila Slesarev <blackdeer1524@yandex.ru>
2022-10-21 19:31:13 +0300

    1.13 done

---
+++ 1/13/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *concat(char **s, int n) {
+    size_t new_string_length = 0;
+
+    for (size_t i = 0; i < n; ++i) {
+        new_string_length += strlen(s[i]);
+    }
+
+    char *result = malloc(sizeof (char) * (new_string_length + 1));
+    result[new_string_length] = '\0';
+    char *res_pointer = result;
+
+    for (size_t i = 0; i < n; ++i) {
+        size_t current_str_length = strlen(s[i]);
+        memmove(res_pointer, s[i], current_str_length);
+        res_pointer += current_str_length;
+    }
+    return result;
+}
+
+
+int main() {
+    char *tests[] = {
+            "0",
+            "12",
+            "345",
+            "6789",
+    };
+    char *concat_res = concat(tests, sizeof (tests) / sizeof tests[0]);
+    printf("%s", concat_res);
+    free(concat_res);
+}
