2d2acfc503eed05ff9d20b59dffb61da93bf7298
Danila Slesarev <blackdeer1524@yandex.ru>
2022-12-29 21:00:25 +0300

    feat!(*): dir rename

--- 3/11/main/include/main.h
+++
@@ -1,6 +0,0 @@
-#ifndef IU_ALGOS_MAIN_H
-#define IU_ALGOS_MAIN_H
-
-#include "lexer.h"
-
-#endif //IU_ALGOS_MAIN_H
--- 3/11/main/src/main.c
+++
@@ -1,7 +0,0 @@
-#include <string.h>
-#include "lexer.h"
-
-int main() {
-    char *sentence = "1 + x - x + (alpha beta gamma) - x";
-    tokenize(sentence, strlen(sentence));
-}
