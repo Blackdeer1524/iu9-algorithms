8ac1793d5d14b226b191545278f3c7734aa848b2
Danila Slesarev <blackdeer1524@yandex.ru>
2023-01-13 18:32:10 +0300

    chore(2/prefixes): submited results

--- 2/prefixes/main.c
+++ 2/prefixes/main.c
@@ -14,9 +14,5 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    if (find_prefixes(str, strlen(str))) {
-        return EXIT_FAILURE;
-    }
-
     return EXIT_SUCCESS;
 }
