7c4c5a0b5f8d889bfd98c615ccfc03e5f5624859
Danila Slesarev <blackdeer1524@yandex.ru>
2023-01-24 19:42:21 +0300

    feat(2/rangepeak): submission

--- 2/rangepeak/submission.c
+++ 2/rangepeak/submission.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <string.h>
-#include <limits.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 
-typedef struct Node{
+typedef struct {
     int value;
+    bool is_peak;
+} BorderInfo;
+
+
+typedef struct Node{    
+    size_t peak_count;
+    size_t l_bound;
+    size_t r_bound;
+
+    BorderInfo leftmost_info;
+    BorderInfo rightmost_info;
 
     struct Node *parent;
     struct Node *l_child;
     struct Node *r_child;
-
-    size_t l_bound;
-    size_t r_bound;
 } Node;
 
 
@@ -25,8 +32,6 @@ void free_segment_tree(Node *root) {
     free_segment_tree(root->r_child);
     free(root);
 }
-
-#define max(x, y) ((x) > (y) ? (x) : (y))
 
 
 static Node *build_tree(int *array, size_t size, size_t l, size_t r) {
@@ -41,7 +46,11 @@ static Node *build_tree(int *array, size_t size, size_t l, size_t r) {
     parent->r_bound=r;
 
     if (l == r) {
-        parent->value=array[l];
+        parent->peak_count = 1;
+        parent->leftmost_info.value = array[l];
+        parent->leftmost_info.is_peak = true;
+        parent->rightmost_info.value = array[l];
+        parent->rightmost_info.is_peak = true;
         return parent;
     } 
 
@@ -53,10 +62,32 @@ static Node *build_tree(int *array, size_t size, size_t l, size_t r) {
     if ((parent->r_child = build_tree(array, size, mid, r)) == NULL) {
         free_segment_tree(parent);
         return NULL;
-    }
-    parent->value = max(parent->l_child->value, parent->r_child->value);
+    }    
+
     parent->l_child->parent = parent;
     parent->r_child->parent = parent;
+    
+    parent->leftmost_info = parent->l_child->leftmost_info;
+    parent->rightmost_info = parent->r_child->rightmost_info;
+
+    parent->peak_count = parent->l_child->peak_count + parent->r_child->peak_count; 
+
+    bool left_peak_is_overwritten  = parent->l_child->rightmost_info.is_peak && parent->r_child->leftmost_info.value  > parent->l_child->rightmost_info.value;
+    bool right_peak_is_overwritten = parent->r_child->leftmost_info.is_peak  && parent->l_child->rightmost_info.value > parent->r_child->leftmost_info.value;
+
+    if (left_peak_is_overwritten || right_peak_is_overwritten) {
+        --parent->peak_count;
+        if (l == r - 1) {
+            if (left_peak_is_overwritten) {
+                parent->leftmost_info.is_peak = false;
+            } else {
+                parent->rightmost_info.is_peak = false;
+            }
+        } else if (l == r - 2 && left_peak_is_overwritten) {
+            parent->leftmost_info.is_peak = false;
+        } 
+    }
+
     return parent;
 }
 
@@ -69,28 +100,45 @@ Node *build_segment_tree(int *array, size_t size) {
 }
 
 
-int get_max(Node *root, size_t l, size_t r, bool *error) {
+size_t get_peak_count(Node *root, size_t l, size_t r, bool *error) {
     if (root == NULL) {
         *error = true;
-        return INT_MIN;
+        return 0;
     }
     
-    if (root->r_bound < l || root->l_bound > r) {
-        return INT_MIN;
-    } else if (l <= root->l_bound && root->r_bound <= r) { 
-        return root->value;
-    } 
-
-    int left_max = get_max(root->l_child, l, r, error);
-    if (*error) {
-        return INT_MIN;
+    if (l <= root->l_bound && root->r_bound <= r) {         // <----------------------------------------
+        return root->peak_count;                            //                                          |
+    }                                                       //                                          |                                                 
+                                                            //                                          | 
+    if (root->l_child == NULL || root->r_child == NULL) {   // Если оба NULL, то должно было сработать это
+        *error = true;
+        return 0;
     }
-    int right_max = get_max(root->r_child, l, r, error);
-    if (*error) {
-        return INT_MIN;
+    
+    size_t total_peak_count = 0;
+    if (l <= root->l_child->r_bound && root->r_child->l_bound <= r) {
+        size_t left_peak_count = get_peak_count(root->l_child, l, r, error);
+        if (*error) {
+            return 0;
+        }
+        size_t right_peak_count = get_peak_count(root->r_child, l, r, error);
+        if (*error) {
+            return 0;
+        }
+        
+        total_peak_count = left_peak_count + right_peak_count;
+        bool left_peak_is_overwritten  = root->l_child->rightmost_info.is_peak && root->r_child->leftmost_info.value  > root->l_child->rightmost_info.value;
+        bool right_peak_is_overwritten = root->r_child->leftmost_info.is_peak  && root->l_child->rightmost_info.value > root->r_child->leftmost_info.value;
+        if (left_peak_is_overwritten || right_peak_is_overwritten) {
+            --total_peak_count;
+        }
+    } else if (l <= root->l_child->r_bound) {
+        total_peak_count = get_peak_count(root->l_child, l, r, error);
+    } else if (root->r_child->l_bound <= r) {
+        total_peak_count = get_peak_count(root->r_child, l, r, error);
     }
 
-    return max(left_max, right_max);
+    return total_peak_count;
 }
 
 
@@ -98,37 +146,50 @@ bool update(Node *root, size_t i, int new_value) {
     if (root == NULL) { 
         return true;
     }
+
     if (root->l_bound == i && root->l_bound == root->r_bound) {
-        root->value = new_value;
+        root->leftmost_info.value = new_value;
+        root->rightmost_info.value = new_value;
         return false; 
     }
 
-    if (root->l_child != NULL) {
-        if (root->l_child->r_bound >= i) {
-            bool error = update(root->l_child, i, new_value); 
-            if (root->r_child != NULL) {
-                root->value = max(root->l_child->value, root->r_child->value);
-            } else {
-                root->value = root->l_child->value;
-            }
-            return error;
-        }
+    if (root->l_child == NULL || root->r_child == NULL) {
+        return true;
     }
 
-    if (root->r_child != NULL) {
-        if (root->r_child->l_bound <= i) {
-            bool error = update(root->r_child, i, new_value);
-            if (root->l_child != NULL) {
-                root->value = max(root->l_child->value, root->r_child->value);
-            } else {
-                root->value = root->r_child->value;
-            }
-            return error;
+    if (root->l_child->l_bound <= i && i <= root->l_child->r_bound) {
+        if (update(root->l_child, i, new_value)) {
+            return true;
+        }
+    } else if (root->r_child->l_bound <= i && i <= root->r_child->r_bound) {
+        if (update(root->r_child, i, new_value)) {
+            return true;
         }
     }
-    return true;
+    
+    root->leftmost_info = root->l_child->leftmost_info;
+    root->rightmost_info = root->r_child->rightmost_info;
+
+    root->peak_count = root->l_child->peak_count + root->r_child->peak_count; 
+
+    bool left_peak_is_overwritten  = root->l_child->rightmost_info.is_peak && root->r_child->leftmost_info.value  > root->l_child->rightmost_info.value;
+    bool right_peak_is_overwritten = root->r_child->leftmost_info.is_peak  && root->l_child->rightmost_info.value > root->r_child->leftmost_info.value;
+
+    if (left_peak_is_overwritten || right_peak_is_overwritten) {
+        --root->peak_count;
+        if (root->l_bound == root->r_bound - 1) {
+            if (left_peak_is_overwritten) {
+                root->leftmost_info.is_peak = false;
+            } else {
+                root->rightmost_info.is_peak = false;
+            }
+        } else if (root->l_bound == root->r_bound - 2 && left_peak_is_overwritten) {
+            root->leftmost_info.is_peak = false;
+        } 
+    }
+
+    return false;
 }
-
 
 
 int main() {
@@ -143,27 +204,29 @@ int main() {
 
     Node *tree = build_segment_tree(array, n);
 
-    char buffer[4];
-    buffer[3] = '\0';
+    char buffer[5];
+    buffer[4] = '\0';
 
-    int c;
     bool error = false;
-    while ((c = getchar()) != EOF) {
+    while (getchar() != EOF) {
         buffer[0] = getchar();
         buffer[1] = getchar();
         buffer[2] = getchar();
 
         if (!strcmp(buffer, "END")) {
             break;
-        } else if (!strcmp(buffer, "MAX")) {
+        } 
+        buffer[3] = getchar();
+        
+        if (!strcmp(buffer, "PEAK")) {
             size_t l, r;
             scanf("%zu %zu", &l, &r);
 
-            int res = get_max(tree, l, r, &error);
+            size_t res = get_peak_count(tree, l, r, &error);
             if (error) {
                 break;
             }
-            printf("%d\n", res);
+            printf("%zu\n", res);
         } else if (!strcmp(buffer, "UPD")) {
             size_t index;
             int new_value;
