e226eb4490952ea7daf2c9919ae698a36c2d209c
Danila Slesarev <blackdeer1524@yandex.ru>
2023-01-25 16:34:56 +0300

    feat(2/rangegcd): tested set_item

--- 2/rangegcd/tests/table_tests.c
+++ 2/rangegcd/tests/table_tests.c
@@ -24,9 +24,26 @@ void test_get_item(void) {
     TEST_ASSERT_EQUAL(3, get_item(&table, 0, 0));
     TEST_ASSERT_EQUAL(1, get_item(&table, 0, 1));
     TEST_ASSERT_EQUAL(1, get_item(&table, 0, 2));
-    TEST_ASSERT_EQUAL(2, get_item(&table, 1, 0));
     TEST_ASSERT_EQUAL(2, get_item(&table, 1, 1));
     TEST_ASSERT_EQUAL(6, get_item(&table, 1, 2));
+}
+
+
+void test_set_item(void) {
+    set_item(&table, 0, 0, 5);
+    TEST_ASSERT_EQUAL(5, get_item(&table, 0, 0));
+
+    set_item(&table, 0, 1, 5);
+    TEST_ASSERT_EQUAL(5, get_item(&table, 0, 1));
+
+    set_item(&table, 0, 2, 5);
+    TEST_ASSERT_EQUAL(5, get_item(&table, 0, 2));
+
+    set_item(&table, 1, 1, 5);
+    TEST_ASSERT_EQUAL(5, get_item(&table, 1, 1));
+
+    set_item(&table, 1, 2, 5);
+    TEST_ASSERT_EQUAL(5, get_item(&table, 1, 2));
 }
 
 
@@ -35,6 +52,7 @@ int main() {
     UNITY_BEGIN();
 
     RUN_TEST(test_get_item);
+    RUN_TEST(test_set_item);
 
     return UNITY_END();
 }
