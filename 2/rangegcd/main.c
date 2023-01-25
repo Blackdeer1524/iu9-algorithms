1c1c462bf97e8fdbcfaec7dde318a93caeb3a819
Danila Slesarev <blackdeer1524@yandex.ru>
2023-01-25 23:54:04 +0300

    feat(2/rangegcd): tested main.c

--- 2/rangegcd/main.c
+++ 2/rangegcd/main.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "table.h"
-
+#include "gcd_matrix.h"
 
 extern inline Table table_build(size_t n_rows, size_t n_cols);
 extern inline void table_free(Table *table);
@@ -9,35 +8,49 @@ extern inline size_t _get_index(Table *table, size_t row, size_t col);
 extern inline table_item_t get_item(Table *table, size_t row, size_t col);
 extern inline void set_item(Table *table, size_t row, size_t col, table_item_t item);
 
-
 int main() {
-    // size_t n;
-    // if (scanf("%zu", &n) != 1) {
-    //     return EXIT_FAILURE;
-    // }
+    size_t n;
+    if (scanf("%zu", &n) != 1) {
+        return EXIT_FAILURE;
+    }
 
-    // int *array = malloc(sizeof(int) * n);
-    // for (size_t i = 0; i < n; ++i) {
-    //     if (scanf("%d", array + i) != 1) {
-    //         free(array);
-    //         return EXIT_FAILURE;
-    //     }
-    //     if (array[i] < 0) {
-    //         array[i] = -array[i];
-    //     }
-    // }
-    
-    
-    size_t n = 10;
-    int array[] = {-3, 1, 5, 10, 13, 26, -52, 0, 10, 0};
-    for (size_t i = 0; i < n; ++i) { 
-        if (array[i] < 0) {
-            array[i] = -array[i];
+    int *array = malloc(n * sizeof(int));
+    for (size_t i = 0; i < n; ++i) {
+        if (scanf("%d", array + i) != 1) {
+            free(array);
+            return EXIT_FAILURE;
         }
     }
 
-    
+    size_t m;
+    if (scanf("%zu", &m) != 1) {
+        free(array);
+        return EXIT_FAILURE;
+    }
 
+    bool error = false;
+    Table tree = get_gcd_table(array, n, &error);
+    if (error) {
+        free(array);
+        table_free(&tree);
+        return EXIT_FAILURE;
+    }
 
-    return EXIT_SUCCESS;
+    int status = EXIT_SUCCESS;
+    for (size_t i = 0; i < m; ++i) {
+        size_t l, r;
+        if (scanf("%zu %zu", &l, &r) != 2) {
+            status = EXIT_FAILURE;
+            break;
+        }
+        int res = interval_gcd(&tree, l, r, &error);
+        if (error || printf("%d\n", res) < 0) { 
+            status = EXIT_FAILURE;
+            break;
+        }
+    }
+
+    free(array);
+    table_free(&tree);
+    return status;
 }
