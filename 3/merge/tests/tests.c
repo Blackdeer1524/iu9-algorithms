dbdf8f43fec158855feaa237ce54563cbb75c224
Danila Slesarev <blackdeer1524@yandex.ru>
2023-01-26 22:54:22 +0300

    feat(3/merge): tested heap minimum retrival

--- 3/merge/tests/tests.c
+++ 3/merge/tests/tests.c
@@ -8,7 +8,11 @@ void setUp(void) {}
 void tearDown(void) {}
 
 
-void test_min_heap_property(Heap *heap) {
+void assert_min_heap_property(Heap *heap) {
+    if (heap->length <= 1) {
+        return;
+    }
+
     for (size_t i = heap->length - 1; i > 0; --i) {
         TEST_ASSERT_LESS_OR_EQUAL(heap->data[i].item, heap->data[parent(i)].item);
     }
@@ -26,7 +30,8 @@ void test_min_heap(void) {
             TEST_ASSERT_FALSE(insert(&heap, t));
         }
         
-        test_min_heap_property(&heap);
+        assert_min_heap_property(&heap);
+        free(heap.data);
     }
     {
         bool error = false;
@@ -39,15 +44,78 @@ void test_min_heap(void) {
             TEST_ASSERT_FALSE(insert(&heap, t));
         }
         
-        test_min_heap_property(&heap);
+        assert_min_heap_property(&heap);
+        free(heap.data);
     }
 }
+
+
+void test_get_min(void) {
+    {
+        bool error = false;
+        Heap heap = build_heap(5, &error);
+        TEST_ASSERT_FALSE(error);
+
+        int test_values[] = {5, 4, 3, 2, 1};
+        for (size_t i = 0; i < sizeof(test_values) / sizeof(test_values[0]); ++i) {
+            HeapItem t = {.item = test_values[i], .source_index = 0};
+            TEST_ASSERT_FALSE(insert(&heap, t));
+        }
+    
+        int res;
+        res = get_minimum(&heap, &error).item;
+        TEST_ASSERT_FALSE(error);
+        TEST_ASSERT_EQUAL(1, res);
+        res = pop_minimum(&heap, &error).item;
+        TEST_ASSERT_FALSE(error);
+        TEST_ASSERT_EQUAL(1, res);
+
+        res = get_minimum(&heap, &error).item;
+        TEST_ASSERT_FALSE(error);
+        TEST_ASSERT_EQUAL(2, res);
+        res = pop_minimum(&heap, &error).item;
+        TEST_ASSERT_FALSE(error);
+        TEST_ASSERT_EQUAL(2, res);
+
+        res = get_minimum(&heap, &error).item;
+        TEST_ASSERT_FALSE(error);
+        TEST_ASSERT_EQUAL(3, res);
+        res = pop_minimum(&heap, &error).item;
+        TEST_ASSERT_FALSE(error);
+        TEST_ASSERT_EQUAL(3, res);
+
+        res = get_minimum(&heap, &error).item;
+        TEST_ASSERT_FALSE(error);
+        TEST_ASSERT_EQUAL(4, res);
+        res = pop_minimum(&heap, &error).item;
+        TEST_ASSERT_FALSE(error);
+        TEST_ASSERT_EQUAL(4, res);
+
+        res = get_minimum(&heap, &error).item;
+        TEST_ASSERT_FALSE(error);
+        TEST_ASSERT_EQUAL(5, res);
+        res = pop_minimum(&heap, &error).item;
+        TEST_ASSERT_FALSE(error);
+        TEST_ASSERT_EQUAL(5, res);
+
+        pop_minimum(&heap, &error);
+        TEST_ASSERT_TRUE(error);
+
+        error = false;
+        get_minimum(&heap, &error);
+        TEST_ASSERT_TRUE(error);
+
+        assert_min_heap_property(&heap);
+        free(heap.data);
+    }
+} 
 
 
 int main() {
     UNITY_BEGIN();
 
     RUN_TEST(test_min_heap);
+    RUN_TEST(test_get_min);
 
     return UNITY_END();
 }
