534c17b411f7d5b8c34b86702567ddc8127444f3
Danila Slesarev <blackdeer1524@yandex.ru>
2023-01-29 03:29:13 +0300

    feat(3/lexavl): submission version

---
+++ 3/lexavl/submission.c
@@ -0,0 +1,465 @@
+#define _GNU_SOURCE
+
+// =========./lib/include/avl.h=========
+
+#ifndef IU_ALGOS_AVL_H
+#define IU_ALGOS_AVL_H
+
+struct node;
+
+typedef struct node node;
+
+typedef enum {
+    NO_INSERT,
+    LEFT_T,
+    RIGHT_T
+} subtree_t;
+
+void print_tree(node *root, char *pref);
+
+void free_tree(node *root);
+
+struct node *create_node(char *key, int height);
+
+int avlget(struct node *root, char *identifier_name);
+
+subtree_t avlinsert(struct node **root, char *item);
+
+#endif //IU_ALGOS_AVL_H
+
+// =========./lib/src/avl.c=========
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// #include "avl.h"
+
+
+// Это - основа. Это, так сказать, база
+// https://www.youtube.com/watch?v=jDM6_TnYIqE
+
+static int new_identifier_index = 0;
+
+typedef struct node {
+    char *identifier_name;
+    int identifier_value;
+    int height;
+    node *left;
+    node *right;
+} node;
+
+
+void print_tree(node *root, char *pref) {
+    printf("<%s> (k:%s, i:%d, h:%d) ", pref, root->identifier_name, root->identifier_value, root->height);
+    size_t pref_length = strlen(pref);
+    if (root->left != NULL) {
+        char *l_pref = malloc(sizeof (char) * (pref_length + 2));
+        strncpy(l_pref, pref, pref_length);
+        l_pref[pref_length] = 'L';
+        l_pref[pref_length + 1] = '\0';
+        print_tree(root->left, l_pref);
+        free(l_pref);
+    }
+    if (root->right != NULL) {
+        char *r_pref = malloc(sizeof (char) * (pref_length + 2));
+        strncpy(r_pref, pref, pref_length);
+        r_pref[pref_length] = 'R';
+        r_pref[pref_length + 1] = '\0';
+        print_tree(root->right, r_pref);
+        free(r_pref);
+    }
+}
+
+
+void free_tree(node *root) {
+    if (root == NULL) {
+        return;
+    }
+
+    free_tree(root->left);
+    free_tree(root->right);
+    free(root->identifier_name);
+    free(root);
+}
+
+
+static int height(node* p) {
+    return (p == NULL) ? -1 : p->height;
+}
+
+
+static int bfactor(node* p) {
+    return height(p->right) - height(p->left);
+}
+
+
+static void fixheight(node* p) {
+    int hl = height(p->left);
+    int hr = height(p->right);
+    if (hl == -1 && hr == -1) {
+        p->height = 0;
+    } else {
+        p->height = (hl > hr ? hl : hr) + 1;
+    }
+}
+
+
+struct node *create_node(char *identifier_name, int height) {
+    struct node *leaf = malloc(sizeof (struct node));
+    leaf->identifier_name = malloc(sizeof(char) * (strlen(identifier_name) + 1));
+    strcpy(leaf->identifier_name, identifier_name);
+    leaf->identifier_value = new_identifier_index++;
+    leaf->left = NULL;
+    leaf->right = NULL;
+    leaf->height = height;
+    return leaf;
+}
+
+
+int avlget(struct node *root, char *identifier_name) {
+    if (root == NULL) {
+        return -1;
+    }
+
+    int cmp = strcmp(root->identifier_name, identifier_name);
+    if (cmp == 0) {
+        return root->identifier_value;
+    } else if (cmp < 0) {
+        return avlget(root->right, identifier_name);
+    } else {
+        return avlget(root->left, identifier_name);
+    }
+}
+
+
+subtree_t avlinsert(struct node **root_ptr, char *identifier_name) {
+    node *root = *root_ptr;
+    if (root == NULL) {
+        *root_ptr = create_node(identifier_name, 0);
+        return NO_INSERT;
+    }
+
+    int cmp = strcmp(root->identifier_name, identifier_name);    
+    if (cmp > 0) {
+        // L
+        subtree_t insertion_direction = avlinsert(&root->left, identifier_name);
+        fixheight(root);
+        int balancing_factor = bfactor(root);
+        if (-2 < balancing_factor && balancing_factor < 2) {
+            return LEFT_T;
+        }
+
+        switch (insertion_direction) {
+            case LEFT_T: {  // LL
+                // swaps root and middle
+                node placeholder = *root;  // full copy
+                node *l_subtree = root->left;
+                node *lr_subtree = l_subtree->right;
+                *root = *l_subtree;
+                *l_subtree = placeholder;
+
+                // root and r_subtree are swapped
+                // root contains r_subtree data now
+                // r_subtree contains root data now
+                root->right = l_subtree;
+                root->right->left = lr_subtree;
+
+                fixheight(root->left);
+                fixheight(root->right);
+                fixheight(root);
+                break;
+            }
+            case RIGHT_T: {  // LR
+                node *l_node = root->left;
+                node *lr_node = l_node->right;
+
+                node *lrl_subtree;
+                node *lrr_subtree;
+                lrl_subtree = lr_node->left;
+                lrr_subtree = lr_node->right;
+
+                node placeholder = *root;
+                *root = *lr_node;
+                *lr_node = placeholder;
+
+                root->right = lr_node;
+                root->right->left = lrr_subtree;
+                root->left = l_node;
+                root->left->right = lrl_subtree;
+                fixheight(root->left);
+                fixheight(root->right);
+                fixheight(root);
+                break;
+            }
+            case NO_INSERT:
+                break;
+            default:
+                exit(1);
+                break;
+        }
+        return LEFT_T;
+    } else if (cmp < 0) {
+        // R
+        subtree_t insertion_direction = avlinsert(&root->right, identifier_name);
+        fixheight(root);
+        int balancing_factor = bfactor(root);
+        if (-2 < balancing_factor && balancing_factor < 2) {
+            return RIGHT_T;
+        }
+
+        switch (insertion_direction) {
+            case LEFT_T: {  // RL
+                node *r_node = root->right;
+                node *rl_node = r_node->left;
+
+                node *rlr_subtree;
+                node *rll_subtree;
+                rlr_subtree = rl_node->right;
+                rll_subtree = rl_node->left;
+
+                node placeholder = *root;
+                *root = *rl_node;
+                *rl_node = placeholder;
+
+                root->left = rl_node;
+                root->left->right = rll_subtree;
+                root->right = r_node;
+                root->right->left = rlr_subtree;
+                fixheight(root->left);
+                fixheight(root->right);
+                fixheight(root);
+                break;
+            }
+            case RIGHT_T: {  // RR
+                // swaps root and middle
+                node placeholder = *root;  // full copy
+                node *r_subtree = root->right;
+                node *rl_subtree = r_subtree->left;
+                *root = *r_subtree;
+                *r_subtree = placeholder;
+
+                // root and r_subtree are swapped
+                // root contains r_subtree data now
+                // r_subtree contains root data now
+                root->left = r_subtree;
+                root->left->right = rl_subtree;
+
+                fixheight(root->left);
+                fixheight(root->right);
+                fixheight(root);
+                break;
+            }
+            case NO_INSERT:
+                break;
+            default:
+                exit(1);
+                break;
+        }
+        return RIGHT_T;
+    }
+    return NO_INSERT;
+}
+
+// =========./lib/include/lexer.h=========
+
+#ifndef IU_ALGOS_LEXER_H
+#define IU_ALGOS_LEXER_H
+
+#include <stdio.h>
+
+typedef enum {
+    CONST,
+    SPEC,
+    IDENT
+}  LEXEM_T;
+
+typedef enum {
+    SPEC_plus,
+    SPEC_minus,
+    SPEC_mul,
+    SPEC_div,
+    SPEC_l_parenthesis,
+    SPEC_r_parenthesis
+} SPEC_T;
+
+
+typedef struct  {
+    LEXEM_T tag;
+    int value;
+} lexem;
+
+
+void tokenize(const char *sentence, size_t sentence_length);
+
+#endif //IU_ALGOS_LEXER_H
+
+// =========./lib/src/lexer.c=========
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+#include <stdlib.h>
+
+// #include "lexer.h"
+// #include "avl.h"
+
+
+static lexem classify_token(
+        const char *token_start,
+        size_t length,
+        int contains_char,
+        node **identifier_tree_root) {
+    if (!length) {
+        fprintf(stderr, "cannot classify token with 0 length\n");
+        exit(1);
+    } else if (length == 1) {
+        lexem spec_lexem;
+        spec_lexem.tag = SPEC;
+        switch (*token_start) {
+            case '+':
+                spec_lexem.value = SPEC_plus;
+                break;
+            case '-':
+                spec_lexem.value = SPEC_minus;
+                break;
+            case '*':
+                spec_lexem.value = SPEC_mul;
+                break;
+            case '/':
+                spec_lexem.value = SPEC_div;
+                break;
+            case '(':
+                spec_lexem.value = SPEC_l_parenthesis;
+                break;
+            case ')':
+                spec_lexem.value = SPEC_r_parenthesis;
+                break;
+            default:
+                goto determine_const_ident;
+        }
+        return spec_lexem;
+    }
+
+    determine_const_ident:;
+
+    lexem const_ident_lexem;
+    int is_first_digit = isdigit(*token_start);
+    if (contains_char && is_first_digit) {
+        fprintf(stderr, "bad identifier: identifiers cannot start with a digit\n");
+        exit(1);
+    } else if (contains_char) {
+        const_ident_lexem.tag = IDENT;
+
+        char *token_buffer = malloc(sizeof (char) * (length + 1));
+        strncpy(token_buffer, token_start, length);
+        token_buffer[length] = '\0';
+        const_ident_lexem.value = avlget(*identifier_tree_root, token_buffer);
+
+        if (const_ident_lexem.value == -1) {
+            avlinsert(identifier_tree_root, token_buffer);
+            const_ident_lexem.value = avlget(*identifier_tree_root, token_buffer);
+            assert(const_ident_lexem.value != -1);
+        }
+        free(token_buffer);
+    } else {
+        const_ident_lexem.tag = CONST;
+        char *token_buffer = malloc(sizeof (char) * (length + 1));
+        strncpy(token_buffer, token_start, length);
+        token_buffer[length] = '\0';
+        const_ident_lexem.value = atoi(token_buffer);
+        free(token_buffer);
+    }
+    return const_ident_lexem;
+}
+
+
+static const char *get_next_token(const char *token_start, size_t *new_token_length, int *contains_chars) {
+    if (token_start == NULL) {
+        fprintf(stderr, "Cannot extract tokens from null pointer\n");
+        exit(1);
+    }
+
+    // skips blanks
+    while (*token_start != '\0' && isblank(*token_start)) {
+        ++token_start;
+    }
+    if (*token_start == '\0') {
+        return NULL;
+    }
+
+    int token_length = 0;
+    int cont_chars = 0;
+    char c;
+    while ((c = *(token_start + token_length)) != '\0' && (isdigit(c) || isalpha(c))) {
+        if (isalpha(c)) {
+            cont_chars = 1;
+        }
+        ++token_length;
+    }
+
+    if (!token_length) {
+        // cannot be \0 because it was checked above
+        *new_token_length = 1;
+        *contains_chars = 0;
+        return token_start;
+    }
+
+//    if (c == '\0') {
+//        --token_length;
+//    }
+
+    *new_token_length = token_length;
+    *contains_chars = cont_chars;
+    return token_start;
+}
+
+
+void tokenize(const char *sentence, size_t sentence_length) {
+    const char *next_token_start = sentence;
+    size_t new_token_length = 0;
+    int contains_chars = 0;
+
+    char *buffer = malloc(sizeof(char) * (sentence_length + 1));
+
+    node *identifier_tree_node = NULL;
+
+    while ((next_token_start = get_next_token(next_token_start, &new_token_length, &contains_chars)) != NULL) {
+        strncpy(buffer, next_token_start, new_token_length);
+        buffer[new_token_length] = '\0';
+        // printf("value: %s\n", buffer);
+
+        lexem next_lexem = classify_token(
+                next_token_start,
+                new_token_length,
+                contains_chars,
+                &identifier_tree_node);
+
+        // printf("<%s> type: %d; value:%d\n", buffer, next_lexem.tag, next_lexem.value);
+        printf("%d %d\n", next_lexem.tag, next_lexem.value);
+        next_token_start += new_token_length;
+    }
+    free_tree(identifier_tree_node);
+    free(buffer);
+}
+
+// ====main.c======================================
+
+#include <string.h>
+#include <stdlib.h>
+// #include "lexer.h"
+
+
+int main() {
+    char *sentence = NULL;
+    size_t allocated_size = 0;
+
+    signed long long actual_length;
+    if ((actual_length = getline(&sentence, &allocated_size, stdin)) == -1) {
+        return EXIT_FAILURE;
+    } 
+    sentence[actual_length - 1] = '\0';
+    tokenize(sentence, strlen(sentence));
+    return EXIT_SUCCESS;
+}
---
+++ 3/ranklist/submission.c
@@ -0,0 +1,466 @@
+// ========./lib/include/skiplist.h=========
+#ifndef LINKED_LIST_H_
+#define LINKED_LIST_H_
+
+#include <stdlib.h>
+
+typedef struct LinkedList LinkedList;
+
+struct LinkedList {
+    char *value;
+    LinkedList *next;    
+};
+
+LinkedList *add_node(LinkedList *prev, char *value);
+
+void free_linked_list(LinkedList *head);
+
+#endif
+
+// ========./lib/src/skiplist.c=========
+
+// #include "linked_list.h"
+
+LinkedList *add_node(LinkedList *prev, char *value) {
+    LinkedList *head = (LinkedList *) malloc(sizeof(LinkedList));
+    if (head == NULL) { 
+        return NULL;
+    }
+    head->next = prev;    
+    head->value = value;
+    return head; 
+}
+
+
+void free_linked_list(LinkedList *head) {
+    while (head != NULL) {
+        LinkedList *next = head->next;
+        free(head->value);
+        free(head);
+        head = next;
+    }
+}
+
+// ========./lib/include/skiplist.h=========
+
+#ifndef SKIP_LIST_H_
+#define SKIP_LIST_H_
+
+#include <stdlib.h>
+#include <stdbool.h>
+// #include "linked_list.h"
+
+typedef struct Node Node;
+
+typedef struct Pair {
+    Node *next;
+    size_t distance;
+} Pair;
+
+
+struct Node {
+    int key;
+    LinkedList *values_list;   // Может быть NULL только в заголовке
+    size_t level;
+    Pair *ptrs_with_distance;   
+};
+
+typedef struct SkipList {
+    size_t max_level;
+    Node *head;
+} SkipList;
+
+
+SkipList *build_skip_list(size_t max_level);
+
+bool lookup(SkipList *list, int key, char **res);
+
+bool insert(SkipList *list, int key, char *word);
+
+bool delete(SkipList *list, int key);
+
+void free_skip_list(SkipList *list);
+
+bool rank(SkipList *list, int key, size_t *res);
+
+#endif
+// ========./lib/src/skiplist.c=========
+#include <limits.h>
+// #include "skip_list.h"
+
+
+SkipList *build_skip_list(size_t max_level) { 
+    SkipList *skip_list;
+    if (!max_level || (skip_list = malloc(sizeof(SkipList))) == NULL) {
+        return NULL;
+    }
+
+    skip_list->head = malloc(sizeof(Node)); 
+    if (skip_list->head == NULL) {
+        free(skip_list);
+        return NULL;
+    }
+    skip_list->max_level = max_level;
+    skip_list->head->key = INT_MIN;
+    skip_list->head->level = max_level;
+    skip_list->head->values_list = NULL;
+    skip_list->head->ptrs_with_distance = (Pair *) malloc(max_level * sizeof(Pair));
+    if (skip_list->head->ptrs_with_distance == NULL) {
+        free(skip_list->head);
+        free(skip_list);
+        return NULL;
+    }
+    for (size_t i = 0; i < max_level; ++i) {
+        skip_list->head->ptrs_with_distance[i].distance = 1;
+        skip_list->head->ptrs_with_distance[i].next = NULL;
+    }
+    return skip_list;
+}
+
+
+void free_skip_list(SkipList *list) {
+    if (list == NULL) {
+        return;
+    }
+    Node *head = list->head;
+    do {
+        Node *next = head->ptrs_with_distance[0].next;
+        free_linked_list(head->values_list);
+        free(head->ptrs_with_distance);
+        free(head);
+        head = next;
+    } while (head != NULL);
+    free(list);
+}
+
+
+static int generate_level(size_t max_level) {
+    size_t new_level = 1;
+    for (size_t c = 1; c < max_level && !(rand() % 2); ++c) {
+        ++new_level;
+    }
+    return new_level;
+}
+
+
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+
+
+bool lookup(SkipList *list, int key, char **res) {
+    if (list == NULL) {
+        *res = NULL; 
+        return true;
+    }
+
+    Node *head = list->head;
+    size_t cur_search_level = list->max_level - 1;
+    while (cur_search_level > 0 && head->key != key) {
+        if (head->ptrs_with_distance[cur_search_level].next == NULL 
+            || head->ptrs_with_distance[cur_search_level].next->key > key) {
+            --cur_search_level;
+        } else {
+            head = head->ptrs_with_distance[cur_search_level].next;
+        }
+    }
+
+    if (!cur_search_level) {
+        while (head->ptrs_with_distance[0].next != NULL && head->ptrs_with_distance[0].next->key <= key) {
+            head = head->ptrs_with_distance[0].next;
+        }
+    }
+
+    if (head->key == key) {
+        *res = head->values_list->value; 
+    } else {
+        *res = NULL; 
+    }
+    return false;
+}
+
+
+bool rank(SkipList *list, int key, size_t *res) {
+    if (list == NULL) {
+        return true;
+    }
+
+    Node *head = list->head;
+    size_t cur_search_level = list->max_level - 1;
+    size_t sequence_number = 0;
+    while (cur_search_level > 0 && head->key != key) {
+        if (head->ptrs_with_distance[cur_search_level].next == NULL 
+            || head->ptrs_with_distance[cur_search_level].next->key > key) {
+            --cur_search_level;
+        } else {
+            sequence_number += head->ptrs_with_distance[cur_search_level].distance;
+            head = head->ptrs_with_distance[cur_search_level].next;
+        }
+    }
+
+    if (!cur_search_level) {
+        while (head->ptrs_with_distance[0].next != NULL && head->ptrs_with_distance[0].next->key <= key) {
+            sequence_number += head->ptrs_with_distance[0].distance;
+            head = head->ptrs_with_distance[0].next;
+        }
+    }
+
+    if (head->key != key) {
+        *res = 0;
+        return true;
+    } 
+    *res = sequence_number; 
+    return false;
+}
+
+
+bool insert(SkipList *list, int key, char *word) {
+    if (list == NULL || word == NULL) {
+        return true;
+    }
+    Pair **update_list = (Pair **) malloc(list->max_level * sizeof(Pair *)); 
+    if (update_list == NULL) {
+        return true;
+    }
+    size_t *sequence_index_per_level = calloc(list->max_level, sizeof(size_t));
+    if (sequence_index_per_level == NULL) {
+        free(update_list); 
+        return true; 
+    }
+
+    for (size_t i = 0; i < list->max_level; ++i) { 
+        update_list[i] = &list->head->ptrs_with_distance[i];
+    }    
+    LinkedList **rightmost_node_value_list = NULL;
+
+    int cur_key = list->head->key;
+    size_t cur_search_level = list->max_level - 1;
+    while (cur_search_level > 0 && cur_key != key) {
+        if (update_list[cur_search_level]->next == NULL 
+            || update_list[cur_search_level]->next->key > key) {
+            sequence_index_per_level[cur_search_level - 1] = sequence_index_per_level[cur_search_level];
+            --cur_search_level;
+        } else {
+            cur_key = update_list[cur_search_level]->next->key;
+            sequence_index_per_level[cur_search_level] += update_list[cur_search_level]->distance;
+            rightmost_node_value_list = &update_list[cur_search_level]->next->values_list;
+            for (size_t i = 0; i <= cur_search_level; ++i) {
+                update_list[i] = &update_list[cur_search_level]->next->ptrs_with_distance[i];  
+            }
+        }
+    }
+
+    if (!cur_search_level) {
+        sequence_index_per_level[0] = sequence_index_per_level[1];
+        while (update_list[0]->next != NULL && update_list[0]->next->key <= key) {
+            cur_key = update_list[cur_search_level]->next->key;
+            rightmost_node_value_list = &update_list[cur_search_level]->next->values_list;
+            update_list[0] = &update_list[0]->next->ptrs_with_distance[0];
+            ++sequence_index_per_level[0];
+        }
+    }
+
+    if (cur_key == key) {
+        if (rightmost_node_value_list == NULL) {
+            free(sequence_index_per_level);
+            free(update_list);
+            return true;
+        }
+        LinkedList *placeholder = add_node(*rightmost_node_value_list, word);
+        if (placeholder == NULL) {
+            free(sequence_index_per_level);
+            free(update_list);
+            return true;            
+        }
+        *rightmost_node_value_list = placeholder;
+        free(sequence_index_per_level);
+        free(update_list);
+        return false;
+    }
+
+    Node *new_node = malloc(sizeof(Node));
+    new_node->key = key;
+    size_t new_node_level = generate_level(list->max_level);
+    new_node->level = new_node_level; 
+    new_node->ptrs_with_distance = (Pair *) malloc(sizeof(Pair) * new_node_level);
+    new_node->values_list = NULL;
+    LinkedList *placeholder = add_node(new_node->values_list, word);
+    if (placeholder == NULL) {
+        free(new_node);
+        free(sequence_index_per_level);
+        free(update_list);
+        return true;
+    }
+    new_node->values_list = placeholder;
+
+    for (size_t i = new_node_level; i < list->max_level; ++i) {
+        ++update_list[i]->distance;
+    }
+    
+    for (size_t i = 0; i < new_node_level; ++i) {
+        new_node->ptrs_with_distance[i].next = update_list[i]->next;
+        new_node->ptrs_with_distance[i].distance = (sequence_index_per_level[i] + update_list[i]->distance) - (sequence_index_per_level[0] + 1) + 1;
+        update_list[i]->next = new_node;
+        update_list[i]->distance = (sequence_index_per_level[0] + 1) - sequence_index_per_level[i];
+    }
+
+    free(sequence_index_per_level);
+    free(update_list);
+    return false;
+}
+
+
+bool delete(SkipList *list, int key) {
+    if (list == NULL) {
+        return true;
+    }
+    Pair **update_list = (Pair **) malloc(list->max_level * sizeof(Pair *)); // list->head->ptrs_with_distance;
+    if (update_list == NULL) {
+        return true;
+    }
+
+    for (size_t i = 0; i < list->max_level; ++i) { 
+        update_list[i] = &list->head->ptrs_with_distance[i];
+    }    
+
+    int cur_key = list->head->key;
+    size_t cur_search_level = list->max_level - 1;
+    while (cur_search_level > 0 && cur_key != key) {
+        if (update_list[cur_search_level]->next == NULL 
+            || update_list[cur_search_level]->next->key >= key) {
+            --cur_search_level;
+        } else {
+            cur_key = update_list[cur_search_level]->next->key;
+            for (size_t i = 0; i <= cur_search_level; ++i) {
+                update_list[i] = &update_list[cur_search_level]->next->ptrs_with_distance[i];  
+            }
+        }
+    }
+
+    while (update_list[0]->next != NULL && update_list[0]->next->key < key) {
+        update_list[0] = &update_list[0]->next->ptrs_with_distance[0];
+    }
+
+    Node *deleting_node = update_list[0]->next;
+    if (deleting_node == NULL || deleting_node->key != key) {
+        free(update_list);
+        return true;
+    }
+
+    for (size_t i = deleting_node->level; i < list->max_level; ++i) {
+        --update_list[i]->distance;
+    }
+    
+    for (size_t i = 0; i < deleting_node->level; ++i) {
+        update_list[i]->next = deleting_node->ptrs_with_distance[i].next;
+        update_list[i]->distance += deleting_node->ptrs_with_distance[i].distance - 1;
+    }
+
+    free(update_list);
+    return false;
+}
+
+// ========./main.c=========================
+
+
+#define  _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// #include "skip_list.h"
+
+
+void print_list(SkipList *list) {
+    Node *start = list->head;
+    do {
+        printf("[%2zu] %11d->", start->level, start->key);
+        if (start->values_list == NULL) {
+            printf("%s", "NULL");
+        } else {
+            printf("\"%s\"", start->values_list->value);
+        }
+        printf("%s", " | ");
+        for (size_t i = 0; i < start->level; ++i) {
+            if (start->ptrs_with_distance[i].next == NULL) {
+                printf("(NULL, %zu) ", start->ptrs_with_distance[i].distance);
+            } else {
+                printf("(%d, %zu) ", start->ptrs_with_distance[i].next->key, start->ptrs_with_distance[i].distance);
+            }
+        }
+        start = start->ptrs_with_distance[0].next;
+        putchar('\n');
+    } while (start != NULL);  
+}
+
+
+int main() {
+    srand(42);
+    SkipList *skip_list = build_skip_list(32);
+    
+    char buffer[8] = {};
+    int status = EXIT_SUCCESS;
+    do {
+        if (scanf("%s", buffer) != 1) {
+            status = EXIT_FAILURE;
+            break;
+        } 
+        if (!strncmp(buffer, "INSERT", 6)) {
+            int k;
+            if (scanf("%d", &k) != 1) {
+                status = EXIT_FAILURE;
+                break;
+            }
+            char *v = NULL;
+            size_t allocated_space_for_input = 0;
+            signed long long actual_length;
+            if ((actual_length = getline(&v, &allocated_space_for_input, stdin)) == -1) {
+                status = EXIT_FAILURE;
+                free(v);
+                break;
+            }
+            v[actual_length - 1] = '\0';
+            if (insert(skip_list, k, v)) {
+                status = EXIT_FAILURE;
+                free(v);
+                break;
+            }
+        } else if (!strncmp(buffer, "DELETE", 6)) {
+            int k;
+            if (scanf("%d", &k) != 1) {
+                status = EXIT_FAILURE;
+                break;
+            }
+            if (delete(skip_list, k)) {
+                status = EXIT_FAILURE;
+                break;
+            }
+        } else if (!strncmp(buffer, "LOOKUP", 6)) {
+            int k;
+            if (scanf("%d", &k) != 1) {
+                status = EXIT_FAILURE;
+                break;
+            }
+            char *res;
+            if (lookup(skip_list, k, &res)) {
+                status = EXIT_FAILURE;
+                break;
+            }
+            printf("%s\n", res);
+        } else if (!strncmp(buffer, "RANK", 4)) {
+            int k;
+            if (scanf("%d", &k) != 1) {
+                status = EXIT_FAILURE;
+                break;
+            }
+            size_t res;
+            if (rank(skip_list, k, &res)) {
+                status = EXIT_FAILURE;
+                break;
+            }
+            printf("%zu\n", res - 1);
+        } else if (!strncmp(buffer, "PRINT", 5)) {
+            print_list(skip_list);
+        }
+    } while (strncmp(buffer, "END", 3));
+
+    free_skip_list(skip_list);
+    return status;
+}
